3c5bec1923c9fe5ed3798c2ef28057b9ddecfee4
MizuExodus <hungvn246@gmail.com>
2026-07-16 10:36:23 +0700

    change discord id

--- functions/fetchlanyard.js
+++ functions/fetchlanyard.js
@@ -19,7 +19,7 @@ export async function onRequestGet({ request, waitUntil }) {
 }
 
 async function fetchData() {
-    const { data } = await fetch('https://api.lanyard.rest/v1/users/773176098552020993').then(r => r.json());
+    const { data } = await fetch('https://api.lanyard.rest/v1/users/871407667501994015').then(r => r.json());
 
     return {
         discord_user: data.discord_user,
--- index.js
+++ index.js
@@ -241,7 +241,7 @@ fetch('/fetchlanyard').then(r => r.json()).then(updateLanyard);
 !function lanyard() {
     const ws = new WebSocket('wss://api.lanyard.rest/socket');
 
-    ws.addEventListener('open', () => ws.send(JSON.stringify({ op: 2, d: { subscribe_to_id: '773176098552020993' } })));
+    ws.addEventListener('open', () => ws.send(JSON.stringify({ op: 2, d: { subscribe_to_id: '871407667501994015' } })));
     ws.addEventListener('error', () => ws.close());
     ws.addEventListener('close', () => setTimeout(lanyard, 1000));
     ws.addEventListener('message', async ({ data }) => {
